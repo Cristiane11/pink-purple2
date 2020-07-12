d2775ef4b58e174e54450dc479f244f52b057ee6
Cristiane11 <bjwmcd@yahoo.com>
2020-07-12 11:21:47 -0400

    update

--- pink-purple/src/components/ui/Entrance.js
+++ pink-purple/src/components/ui/Entrance.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme=>({
+  button:{
+      ...theme.mixins.button,
+      color:'white',
+      fontFamily:"Playfair",
+      backgroundColor:"#590EED"
+  }
+}));
+
 
 
   function Entrance(){
+    const classes = useStyles()
     return(
        <div>
-         <Button variant="contained" color="secondary">
+         <Button variant="contained" color="secondary"className={classes.button}>
         About Me
         </Button>
        </div> 
