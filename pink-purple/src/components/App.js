ee416467fde8703d9562561222ae6fcade94e676
Cristiane11 <bjwmcd@yahoo.com>
2020-07-12 07:40:08 -0400

    update

--- pink-purple/src/components/App.js
+++ pink-purple/src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './ui/Header';
 import {ThemeProvider } from '@material-ui/core/styles';
 import theme from './ui/Theme';
+import Entrance from './ui/Entrance';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header/>
+      <Entrance/>
       Hello
 
     </ThemeProvider>
---
+++ pink-purple/src/components/ui/Entrance.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+  function Entrance(){
+    return(
+       <div>
+         <Button variant="contained" color="secondary">
+        About Me
+        </Button>
+       </div> 
+    )
+}
+export default Entrance;
